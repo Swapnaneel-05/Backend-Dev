9e9e994d1aa19df5a008a08a7360052c7255051d
Swapnaneel-05 <swapnaneelbhadra@gmail.com>
2026-03-23 15:41:33 +0530

    [Swapnaneel] Added: Crud Operations in Server in MongoDb

---
+++ mongoCrud-02/controllers/userController.js
@@ -0,0 +1,14 @@
+import user from "../model/UserSchema.js";
+
+export const getAll = async(req,res)=>{
+    try {
+        const users = await user.find();
+
+        res.status(200).json({
+            success:true,
+            users
+        })
+    } catch (error) {
+        console.log(error);
+    }
+}
